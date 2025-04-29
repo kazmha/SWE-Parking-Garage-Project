8d1fea29677746473039dd394cd91c630315dfda
hubao <kazmha@farmingdale.edu>
2025-04-29 18:57:37 -0400

    addcar fxml and controller

---
+++ src/main/java/com/example/parkinggaragemanagementsystem/addCarController.java
@@ -0,0 +1,31 @@
+package com.example.parkinggaragemanagementsystem;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class addCarController {
+
+    @FXML
+    private Button addCarButton;
+
+    @FXML
+    private TextField name_input;
+
+    @FXML
+    private TextField spotId_input;
+
+    @FXML
+    private TextField timeEntered_input;
+
+    @FXML
+    private TextField typeOfVehicle_input;
+
+    @FXML
+    void addCar(ActionEvent event) {
+
+    }
+
+}
+
